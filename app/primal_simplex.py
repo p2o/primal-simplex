d8a6c3a99ac6e167a92365ef6eec0275be29330c
p2o <ricardo@p2o.com.br>
2021-10-26 17:38:08 -0300

    adicionado alguns comentarios na fase I e na fase II

--- app/primal_simplex.py
+++ app/primal_simplex.py
@@ -7,27 +7,38 @@ def simplexFii(matrizA,bases,naoBase,vetorC,vetorB):
     cn = vetorC[naoBase]
     b = vetorB
 
+    # Passo 1: {cálculo da solução básica}
     x = np.linalg.solve(B,b)
+
+    # 2.1) {cálculo do vetor multiplicador simplex}
     lamb = np.linalg.solve(np.transpose(B),cb)
 
+    # 2.2) {custos relativos}
     hat_c = []
     for i in range(N.shape[1]):
         hat_c.append(cn[i,0] - np.dot(np.transpose(lamb),N[:,i])[0])
 
+    # 2.3) {escolha da variável a entrar na base}
     hat_c_min_i = np.argmin(hat_c)
     entra_na_base = naoBase[hat_c_min_i]
 
+    # Passo 3: {teste de otimalidade}
     if hat_c[hat_c_min_i] >= 0 and all(x>=0):
         x_r = [0] * matrizA.shape[1]
         for i,j in enumerate(bases):
             x_r[j] = x[i][0]
         return [1,x_r] #solução ótima
 
+    # Passo 4: {cálculo da direção simplex}
     y = np.linalg.solve(B,N[:,hat_c_min_i])
 
+    # Passo 5: {determinar passo e variável a sair da base}
+
+    # Se y <= 0, então PARE = VERDADE {problema não tem solução ótima finita}
     if max(y) <= 0:
         return [2,x] #sem solução ótima finita
 
+    # Caso contrário, determine a variável a sair da base
     hat_epi = []
     hat_epi_i = []
     for i in range(len(y)):
@@ -38,6 +49,7 @@ def simplexFii(matrizA,bases,naoBase,vetorC,vetorB):
     hat_epi_min_i = hat_epi_i[np.argmin(hat_epi)]
     sai_da_base = bases[hat_epi_min_i]
 
+    # Passo 6: {atualização: nova partição básica}
     bases[hat_epi_min_i] = entra_na_base
     naoBase[hat_c_min_i] = sai_da_base
 
@@ -58,6 +70,7 @@ class PrimalSimplex:
         id_columns_found = []
         matriz_B_to_find = np.identity(tamanho_base)
 
+        # Verifica se a matriz A possui base Identidade para usar como partição básica factível inicial
         for col_B in range(tamanho_base):
             for col_A in A_columns:
                 if all(self.A[:,col_A] == matriz_B_to_find[:,col_B]):
@@ -69,6 +82,8 @@ class PrimalSimplex:
                 self.nbase = [i for i in range(self.A.shape[1]) if i not in self.base]
                 break
 
+        # Se a quantidade de colunas encontradas para base é menor que o tamanho necessário para a base então:
+        # é criado um problema auxiliar para encontrar uma base inicial factível utilizando a fase II
         if len(id_columns_found) < tamanho_base:
             columns_to_add = np.asarray([matriz_B_to_find[i] for i in range(tamanho_base) if i not in id_columns_found]).T
             A_aux = np.concatenate((self.A,columns_to_add), axis=1)
